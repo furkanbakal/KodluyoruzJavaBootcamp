f1c475cbac0bc28866bac98c2b8a35070d65336e
Furkan Bakal <f.bakal1997@gmail.com>
2021-06-18 20:34:47 +0300

    Daire alan çevre hesaplaması

--- DaireAlanCevre/src/Main.java
+++ DaireAlanCevre/src/Main.java
@@ -7,16 +7,20 @@ public class Main {
 		
 		int r;
 		double pi=3.14;
-		
+	
 		Scanner input=new Scanner(System.in);
 		
-		System.out.println("Dairenin yarýçapýný giriniz...");
+		System.out.print("Dairenin yarýçapýný giriniz...");
 		r=input.nextInt();
+		System.out.print("Daire diliminin açýsýný giriniz: ");
+		int aci=input.nextInt();
 		double alan=pi*r*r;
 		double cevre=2*pi*r;
+		double daireDilimAlani=(pi*(r*r)*aci)/360;
 		
 		System.out.print("Dairenin alaný: "+alan);
 		System.out.print("Dairenin çevresi: "+cevre);
+		System.out.println("Daire diliminin alaný:"+daireDilimAlani);
 		
 	}
 
