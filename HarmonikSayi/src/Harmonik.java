db5004996d3f4f32f80c720e790acb8c2b304aa3
Furkan Bakal <f.bakal1997@gmail.com>
2021-06-20 21:10:49 +0300

    Girilen sayının harmonik serisini bulan program

---
+++ HarmonikSayi/src/Harmonik.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Harmonik {
+
+	public static void main(String[] args) {
+		// Girilen sayının harmonik serisini bulan program
+		
+		 Scanner input = new Scanner(System.in);
+	        System.out.println("N sayısını giriniz :");
+	        double n = input.nextInt();
+	        double result = 0;
+	        while (n > 0) {
+	            result += (1 / n);
+	            n--;
+	        }
+
+	        System.out.println(result);
+	    }
+
+	}
+
+
