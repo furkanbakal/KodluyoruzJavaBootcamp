0d9928d1f8a9108297b7860f5774db4f67b41e43
Furkan Bakal <f.bakal1997@gmail.com>
2021-06-23 00:38:14 +0300

    Klavyeden girilen değer kadar fibonacci serisi yazdıran program

---
+++ FibonacciSerisi/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Klavyeden girilen deðer aralýðýnda fibonacci serisi bulan program
+		
+		  Scanner reader = new Scanner(System.in);
+	        int n1=0,n2=1,n3,i,n;
+	        System.out.print("N Sayýsýný Girin:");
+	        n = reader.nextInt();
+	        System.out.print(n1+" "+n2);
+	  
+			 for(i=2;i<n;++i)
+			 {  
+			  n3=n1+n2;  
+			  System.out.print(" "+n3);  
+			  n1=n2;  
+			  n2=n3;  
+			    }
+			 System.out.println();
+	}
+
+}
