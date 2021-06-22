e7fc6eccd1ca0adc761841c20223bc5c309c1b6d
Furkan Bakal <f.bakal1997@gmail.com>
2021-06-23 00:32:53 +0300

    1 İLE 100 arasındaki asal sayıları bulan program

---
+++ AsalSayilar/src/Main.java
@@ -0,0 +1,30 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		//1 ile 100 arasýndaki asal sayýlarý bulan program
+		
+		int sayac = 0;
+        for(int sayi=2;sayi<=100;sayi++)
+        {
+            int kontrol = 0;
+            for (int i = 2; i < sayi; i++)
+            {
+               if (sayi % i == 0)
+                {
+                    kontrol = 1;
+                    break;
+                } 
+            }
+                
+            if(kontrol==0)
+            {
+                System.out.print(sayi+" ");
+                sayac++; 
+            }
+        }
+
+	}
+
+}
